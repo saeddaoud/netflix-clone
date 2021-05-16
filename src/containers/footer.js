75aaf2e3446c9d06b814217c32472652fea3a287
Saed Daoud <eng.saed.daoud@gmail.com>
2021-05-15 20:14:53 -0400

    created the footer component

---
+++ src/containers/footer.js
@@ -0,0 +1,44 @@
+import { Footer } from '../components';
+
+const FooterContainer = () => {
+  return (
+    <Footer>
+      <Footer.Title>Questions? Contact us</Footer.Title>
+      <Footer.Break />
+      <Footer.Row>
+        <Footer.Column>
+          <Footer.Link href='#'>FAQ</Footer.Link>
+          <Footer.Link href='#'>Ways to Watch</Footer.Link>
+          <Footer.Link href='#'>Netflix Originals</Footer.Link>
+          <Footer.Link href='#'>Investers Relations</Footer.Link>
+          <Footer.Link href='#'>Coorperte Information</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href='#'>FAQ</Footer.Link>
+          <Footer.Link href='#'>Ways to Watch</Footer.Link>
+          <Footer.Link href='#'>Netflix Originals</Footer.Link>
+          <Footer.Link href='#'>Investers Relations</Footer.Link>
+          <Footer.Link href='#'>Coorperte Information</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href='#'>FAQ</Footer.Link>
+          <Footer.Link href='#'>Ways to Watch</Footer.Link>
+          <Footer.Link href='#'>Netflix Originals</Footer.Link>
+          <Footer.Link href='#'>Investers Relations</Footer.Link>
+          <Footer.Link href='#'>Coorperte Information</Footer.Link>
+        </Footer.Column>
+        <Footer.Column>
+          <Footer.Link href='#'>FAQ</Footer.Link>
+          <Footer.Link href='#'>Ways to Watch</Footer.Link>
+          <Footer.Link href='#'>Netflix Originals</Footer.Link>
+          <Footer.Link href='#'>Investers Relations</Footer.Link>
+          <Footer.Link href='#'>Coorperte Information</Footer.Link>
+        </Footer.Column>
+      </Footer.Row>
+      <Footer.Break />
+      <Footer.Text>Netflix Canada</Footer.Text>
+    </Footer>
+  );
+};
+
+export default FooterContainer;
