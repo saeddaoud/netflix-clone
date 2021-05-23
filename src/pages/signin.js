3bb86d904a190eaa3007cd706f2ee9c7da770b12
Saed Daoud <eng.saed.daoud@gmail.com>
2021-05-23 02:50:42 -0400

    created the sign in page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,46 @@
+import { useState } from 'react';
+import { Form } from '../components';
+import { FooterContainer, HeaderContainer } from '../containers';
+import * as ROUTES from '../constants/routes';
 const Signin = () => {
-  return <h1>Signin page</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted');
+  };
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSubmit}>
+            <Form.Input
+              placeholder='email'
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Input
+              placeholder='password'
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Form.Text>
+              Do not have an account?{' '}
+              <Form.Link to={ROUTES.SIGN_UP}>Register here</Form.Link>
+            </Form.Text>
+            <Form.Submit type='submit'>Submit</Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 };
 
 export default Signin;
